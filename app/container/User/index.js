cd5147fe214fc286f2533c1d452fed056e647aea
binyellow <591112995@qq.com>
2017-09-17 08:22:01 +0800

    user

--- app/container/User/index.js
+++ app/container/User/index.js
@@ -2,14 +2,21 @@ import React from 'react'
 import Header from '../../component/Header'
 import {hashHistory} from 'react-router'
 import {connect} from 'react-redux'
+import UserInfo from '../../component/UserInfo'
+import OrderList from './subpage/OrderList'
 class User extends React.Component{
     constructor(props,state){
         super(props,state);
     }
     render(){
+        const userinfo = this.props.userinfo
+        console.log(userinfo.username)
+        console.log(userinfo.cityName)
         return(
             <div>
                 <Header title="用户中心" backRouter="/"/>
+                <UserInfo username={userinfo.username} city={userinfo.cityName}/>
+                <OrderList username={userinfo.username}/>
             </div>
         )
     }
---
+++ mock/orderlist/orderList.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: Date.now(),
+        img: 'http://orpwhzbuc.bkt.clouddn.com/138012-20161016201638030-473660627.png',
+        title: '汉堡大王',
+        count: 3,
+        price: '167',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'http://orpwhzbuc.bkt.clouddn.com/138012-20161016201708124-1116595594.png',
+        title: '麻辣香锅',
+        count: 1,
+        price: '188',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'http://orpwhzbuc.bkt.clouddn.com/fandian.png',
+        title: '好吃自出餐',
+        count: 2,
+        price: '110',
+        commentState: 2
+    }
+]
